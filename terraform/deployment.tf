823b542e1e1107b97c35ce151b1e43c84dcd8566
mohamed <mohamed@example.com>
2026-02-09 14:36:43 +0200

    Add Mongo URI secrets for Terraform and GitOps

--- terraform/deployment.tf
+++ terraform/deployment.tf
@@ -1,10 +1,30 @@
+resource "kubernetes_secret_v1" "app_secrets" {
+  metadata {
+    name      = "devops-app-secrets"
+    namespace = "default"
+  }
+
+  data = {
+    MONGO_URI = "mongodb+srv://appuser:app-password@cluster0.example.mongodb.net/appdb"
+  }
+
+  type = "Opaque"
+}
+
 resource "kubernetes_deployment_v1" "app" {
+  depends_on = [
+    kubernetes_secret_v1.app_secrets
+  ]
+
   metadata {
     name = "devops-minikube-app"
     labels = {
       app = "devops-minikube-app"
     }
   }
+ 
+
+
 
   spec {
     replicas = 2
@@ -37,6 +57,19 @@ resource "kubernetes_deployment_v1" "app" {
             name  = "PORT"
             value = "3000"
           }
+          
+          env {
+          name = "MONGO_URI"
+
+          value_from {
+            secret_key_ref {
+              name = kubernetes_secret_v1.app_secrets.metadata[0].name
+              key  = "MONGO_URI"
+            }
+          }
+        }
+
+
         }
       }
     }
