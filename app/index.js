3d5e47ccc75f4d4416f347a1c3bfedeebfcd7280
mohamed <mohamed@example.com>
2026-02-11 12:27:08 +0200

    Update app message to test full CI/CD flow

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello from DevOps Minikube project',
+    message: 'Hello from DevOps Minikube project1',
   });
 });
 
